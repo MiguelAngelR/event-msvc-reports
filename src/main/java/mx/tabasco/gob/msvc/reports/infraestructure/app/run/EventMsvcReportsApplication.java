069c761bca4fc027c3532c78c345a6cbd74ecf9d
Angel <miguel.rios.alonso@outlook.com>
2025-04-10 15:29:09 -0500

    add: decorators

--- src/main/java/mx/tabasco/gob/msvc/reports/infraestructure/app/run/EventMsvcReportsApplication.java
+++ src/main/java/mx/tabasco/gob/msvc/reports/infraestructure/app/run/EventMsvcReportsApplication.java
@@ -2,8 +2,18 @@ package mx.tabasco.gob.msvc.reports.infraestructure.app.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableFeignClients(basePackages = "mx.tabasco.gob.msvc.reports.data.datasource.remote")
+@SpringBootApplication()
+@ComponentScan("mx.tabasco.gob")
+@EnableDiscoveryClient
+@EnableJpaRepositories("mx.tabasco.gob.msvc.reports.data")
+@EntityScan({"mx.tabasco.gob.msvc.reports.data.entities.db"})
 public class EventMsvcReportsApplication {
 
     public static void main(String[] args) {
